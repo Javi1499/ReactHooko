90be8dc78ef3f1043184ff9f8c7d188912002de2
Javi1499 <fruiz1@ucol.mx>
2021-05-27 00:11:47 -0500

    Primercommit de prueba

---
+++ Repaso JS/js/app.js
@@ -0,0 +1,13 @@
+//variables con var
+var aprendiendo = "Esta es una prueba"
+
+
+
+//Variables con const
+const aprendiendo = "JavvaScript"
+
+//variables con let
+
+let estaESLet
+
+//Un Scope es hasta que punto un valor de una vasriable esta o no disponible
---
+++ Repaso JS/js/templates.js
@@ -0,0 +1,4 @@
+const nombre = "Javier";
+const trabajo = "Desarrollador de  software"
+
+console.log(`Hola soy javier ${nombre} y trabajo como ${trabajo}`);
